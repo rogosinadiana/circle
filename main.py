e4f397f42dcf96bfddf6127442b3a6b15d9a69f0
DI <diana.rogosina@gmail.com>
2022-12-09 21:55:43 +0300

    V1

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ def circle(X,X0,Y,Y0,R,mas):
             if abs((i-X0)**2+(j-Y0)**2)<=R**2 and abs((i-X0)**2+(j-Y0)**2)>=(R-1)**2:
                 mas[i][j]=1
 def display(X,Y,mas):
-
     for i in range(0,len(mas)):
         for j in range(0,len(mas[i])):
             print(mas[i][j],end=' ')
         print()
-
 def main():
     X=int(input('ЧИСЛО СТРОК (X):'))
     Y=int(input('ЧИСЛО СТОЛБЦОВ (Y):'))
